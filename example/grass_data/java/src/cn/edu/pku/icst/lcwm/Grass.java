6962c9c50e271364e1b1eb269cf20692b04ff115
Chen Yufei <tsjzadmin@gmail.com>
2016-10-28 00:01:09 +0800

    Adapt java and python api for multi platform.

--- example/grass_data/java/src/cn/edu/pku/icst/lcwm/Grass.java
+++ example/grass_data/java/src/cn/edu/pku/icst/lcwm/Grass.java
@@ -7,11 +7,39 @@ import com.sun.jna.Native;
  * Created by draplater on 16-10-23.
  */
 public interface Grass extends Library {
-    Grass INSTANCE = (Grass) Native.loadLibrary("grass", Grass.class);
+    Grass INSTANCE = (Grass) Native.loadLibrary(GrassLibraryNameGetter.libraryName,
+            Grass.class);
     int UTF8 = 65001;
     int create_segmentor_ctx(String featureFile, String dictFile);
     void delete_segmentor_ctx(int idx);
     void train_segmentor_ctx(String train_file, String feature_file, String dict_file, int times, int encoding);
     void seg_file_with_ctx(int idx, String input_file, String output_file, int encoding);
     String seg_string_with_ctx(int idx, String input, int encoding);
+}
+
+class GrassLibraryNameGetter {
+    static String libraryName;
+    static {
+        String osName = System.getProperty("os.name").toLowerCase();
+        String osArch = System.getProperty("os.arch").toLowerCase();
+        if(osName.contains("win")) {
+            osName = "win";
+        } else if(osName.contains("linux")) {
+            osName = "linux";
+        } else {
+            throw new IllegalStateException(
+                    String.format("Your system %s-%s is unsupported.", osName, osArch));
+        }
+
+        if(osArch.contains("64")) {
+            osArch = "64";
+        } else if(osArch.contains("86") || osArch.contains("32")) {
+            osArch = "32";
+        } else {
+            throw new IllegalStateException(
+                    String.format("Your system %s-%s is unsupported.", osName, osArch));
+        }
+
+        libraryName = "grass-" + osName + osArch;
     }
+}
--- example/grass_data/python/grass.py
+++ example/grass_data/python/grass.py
@@ -2,8 +2,28 @@
 ## encoding: utf-8
 from __future__ import unicode_literals, print_function
 import ctypes
+import sys
+import platform
 
-grass = ctypes.cdll.LoadLibrary("./lib/libgrass.so")
+
+def detect_lib_name():
+    """
+    Select corresponding libgrass
+    :return: library file name
+    """
+    if sys.platform in ['win32', 'cygwin']:
+        system = "win"
+        ext = 'dll'
+    elif sys.platform.startswith("linux"):
+        system = "linux"
+        ext = 'so'
+    else:
+        raise SystemError("Your system ({}) is not supported.".format(sys.platform))
+    bit = platform.architecture()[0][:2] # 32 or 64
+    return "libgrass-{}{}.{}".format(system, bit, ext)
+
+
+grass = ctypes.cdll.LoadLibrary("./lib/" + detect_lib_name())
 grass.seg_string_with_ctx.restype = ctypes.c_char_p
 UTF8 = 65001
 
@@ -12,21 +32,13 @@ class Segmentor(object):
     def __init__(self, feature_file, dict_file):
         self.segmentor = grass.create_segmentor_ctx(feature_file, dict_file)
 
-    def seg_string(self, input_str):
+    def seg_string(self, input_str, encoding=b"UTF-8"):
         if isinstance(input_str, unicode):
-            input_str = input_str.encode(b"UTF-8")
-        return grass.seg_string_with_ctx(self.segmentor, input_str, UTF8)
+            input_str = input_str.encode(encoding)
+        return grass.seg_string_with_ctx(self.segmentor, input_str, UTF8).decode(encoding)
 
     def __del__(self):
         grass.delete_segmentor_ctx(self.segmentor)
 
 
-if __name__ == "__main__":
-    segmentor = Segmentor(b"../feature/segment.feat", b"../feature/segment.dic")
-    print(segmentor.seg_string(
-        """
-包姌是五月天的铁杆粉丝，今年6月，得知偶像将在10月在上海开演唱会，她立刻在“牛魔王票务网”上订了张10月4日的演唱会看台票，票面价为555元，该网站售价780元。
-“门票被加价225元，但因为是网站明码标价，所以也没有过多质疑”，包姌告诉记者，座位号不是自选的，而是票务方给分配的。后来，她收到牛魔王票务寄来的门票，座位号是“19台2层39排11座”，演出时间10月4日19点。
-10月3号晚上7点，包姌为了看到第二天期待已久的演唱会，特意从江苏淮安坐车赶往上海，因为堵车，坐了13个小时，次日早晨才到。
-        """
-    ))
+
