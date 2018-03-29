d904b486515bd26c0bd83aac6a8bf4b797a4be39
Chen Yufei <yufei.chen@pku.edu.cn>
2018-03-29 23:17:04 +0800

    ner tag with position

--- java/src/main/java/cn/edu/pku/icst/lcwm/grass/GrassNER.java
+++ java/src/main/java/cn/edu/pku/icst/lcwm/grass/GrassNER.java
@@ -9,8 +9,14 @@ public interface GrassNER extends Library {
     interface callback_t extends Callback {
         void invoke(String word, String tag);
     }
-    GrassNER INSTANCE = (GrassNER) LibraryLoader.loadLibraryInJAR(GrassNER.class, "libgrass_ner");
+
+    interface callback_position_t extends Callback {
+        void invoke(int start, int end, String word, String tag);
+    }
+
+    GrassNER INSTANCE = (GrassNER) LibraryLoader.loadLibraryInJAR(GrassNER.class, "libgrass2");
     NativeLong create_ner_parser(String featureInput, String dictPath);
     int parse_string_with_ner_parser(NativeLong context, String inputString, callback_t fn);
+    int parse_string_with_ner_parser_withposition(NativeLong context, String inputString, callback_position_t fn);
     int delete_ner_parser(NativeLong context);
 }
--- java/src/main/java/cn/edu/pku/icst/lcwm/grass/NERTagger.java
+++ java/src/main/java/cn/edu/pku/icst/lcwm/grass/NERTagger.java
@@ -4,8 +4,6 @@ import com.sun.jna.NativeLong;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOError;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,10 +11,18 @@ public class NERTagger {
     public class Pair {
         private final String word;
         private final String tag;
+        private final int start;
+        private final int end;
 
-        Pair(String word, String tag) {
+        Pair(String word, String tag, int start, int end) {
             this.word = word;
             this.tag = tag;
+            this.start = start;
+            this.end = end;
+        }
+
+        Pair(String word, String tag) {
+            this(word, tag, -1, -1);
         }
 
         public String getWord() {
@@ -27,23 +33,31 @@ public class NERTagger {
             return tag;
         }
 
+        public int getStart() {
+            return start;
+        }
+
+        public int getEnd() {
+            return end;
+        }
+
         @Override
         public String toString() {
-            return String.format("%s/%s", word, tag);
+            return String.format("%s/%s(%d,%d)", word, tag, start, end);
         }
     }
 
     private NativeLong context;
 
     public NERTagger(String featureFile, String dictFile) throws FileNotFoundException {
-        if(!new File(featureFile).exists()) {
+        if (!new File(featureFile).exists()) {
             throw new FileNotFoundException(featureFile);
         }
-        if(dictFile != null && !new File(dictFile).exists()) {
+        if (dictFile != null && !new File(dictFile).exists()) {
             throw new FileNotFoundException(dictFile);
         }
 
-        if(dictFile == null) {
+        if (dictFile == null) {
             dictFile = "";
         }
         context = GrassNER.INSTANCE.create_ner_parser(featureFile, dictFile);
@@ -55,8 +69,9 @@ public class NERTagger {
 
     public List<Pair> tagSentence(String sentence) {
         final ArrayList<Pair> results = new ArrayList<>();
-        GrassNER.callback_t callback = (word, tag) -> results.add(new Pair(word, tag));
-        int code = GrassNER.INSTANCE.parse_string_with_ner_parser(context, sentence, callback);
+        GrassNER.callback_position_t callback = (start, end, word, tag) -> results.add(
+                new Pair(word, tag, start - 1, end - 1));
+        int code = GrassNER.INSTANCE.parse_string_with_ner_parser_withposition(context, sentence, callback);
         return results;
     }
 
