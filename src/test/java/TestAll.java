0e4e3e6be33d714c80759700d894b16fff673b16
Chen Yufei <yufei.chen@pku.edu.cn>
2018-03-29 23:17:04 +0800

    ner tag with position

--- src/test/java/TestAll.java
+++ src/test/java/TestAll.java
@@ -5,33 +5,33 @@ import java.util.List;
 
 public class TestAll {
         public static void main(String[] args) throws FileNotFoundException {
-        Segmentor s = new Segmentor("segment.feat",
-                "segment.dic");
-        String[] segResult = s.segString(
-                "在第31届夏季奥林匹克运动会上，中国女子排球队永不放弃、拼搏到底，" +
-                        "时隔12年重返世界之巅，中国女排姑娘们展现出了一辈辈传承至" +
-                        "今的女排精神，再一次感动了国人、振奋了国人。");
-        System.out.println(segResult.length);
-        for(String i : segResult) {
-            System.out.print(i + " ");
-        }
-
-        POSTagger p = new POSTagger("postag.feat");
-        Grass.WordWithTag[] posResult = p.tagSentence(segResult);
-        for(Grass.WordWithTag i : posResult) {
-            System.out.print(i + " ");
-        }
-        System.out.print("\n");
-
-        SyntaxParser syn = new SyntaxParser("syntax.feat");
-        String syntaxTree = syn.parse(posResult);
-        System.out.println(syntaxTree);
-
-        SemanticParser sem = new SemanticParser(
-                "semantic.feat",
-                "syntax.feat");
-        String semGraph = sem.parse(posResult);
-        System.out.println(semGraph);
+//        Segmentor s = new Segmentor("segment.feat",
+//                "segment.dic");
+//        String[] segResult = s.segString(
+//                "在第31届夏季奥林匹克运动会上，中国女子排球队永不放弃、拼搏到底，" +
+//                        "时隔12年重返世界之巅，中国女排姑娘们展现出了一辈辈传承至" +
+//                        "今的女排精神，再一次感动了国人、振奋了国人。");
+//        System.out.println(segResult.length);
+//        for(String i : segResult) {
+//            System.out.print(i + " ");
+//        }
+//
+//        POSTagger p = new POSTagger("postag.feat");
+//        Grass.WordWithTag[] posResult = p.tagSentence(segResult);
+//        for(Grass.WordWithTag i : posResult) {
+//            System.out.print(i + " ");
+//        }
+//        System.out.print("\n");
+//
+//        SyntaxParser syn = new SyntaxParser("syntax.feat");
+//        String syntaxTree = syn.parse(posResult);
+//        System.out.println(syntaxTree);
+//
+//        SemanticParser sem = new SemanticParser(
+//                "semantic.feat",
+//                "syntax.feat");
+//        String semGraph = sem.parse(posResult);
+//        System.out.println(semGraph);
 
         String inputString = "5个常任理事国: 中国、法国、俄罗斯、英国、美国。";
         NERTagger tagger = new NERTagger("nerfeat_pku");
